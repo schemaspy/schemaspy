98463426bb9e493cde6ba2fc6294d190460b66b7
Rafal Kasa <rafalkasa@gmail.com>
2017-01-31 12:50:22 +0100

    start the process of documenting SchemaSpy - new documentation

--- docs/build/html/_sources/index.rst.txt
+++ docs/build/html/_sources/index.rst.txt
@@ -1,15 +1,11 @@
-.. SchemaSpy documentation master file, created by
-   sphinx-quickstart on Tue Jan 17 23:22:10 2017.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to SchemaSpy's documentation!
-=====================================
+.. include:: home.rst
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 4
+   
+   home
+   installation
    :caption: Contents:
-
 
 
 Indices and tables
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
 
 # -- General configuration ------------------------------------------------
@@ -82,7 +83,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
