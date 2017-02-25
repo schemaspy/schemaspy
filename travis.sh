692534d69c1b999bbd413ce3bdc81836cb2c837a
Rafal Kasa <rafalkasa@gmail.com>
2017-02-25 14:24:30 +0100

    nie jest możliwe zrobienie sonara dla external fork branches

--- travis.sh
+++ travis.sh
@@ -13,7 +13,7 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	#
 	# Analysis is done only on master so that build of branches don't push analyses to the same project and therefore "pollute" the results
 	echo "Starting analysis by SonarQube..."
-	mvn clean -e org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -B -e -V \
+	mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -B -e -V \
 		-Dsonar.host.url=$SONAR_HOST_URL \
 		-Dsonar.login=$SONAR_TOKEN
 
@@ -24,13 +24,17 @@ elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
 	# coming from outside repositories
 	# http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
 	# That's why the analysis does not need to be executed if the variable GITHUB_TOKEN is not defined.
-	echo "Starting Pull Request analysis by SonarQube..."
-	mvn clean -e package sonar:sonar -B -e -V \
+	echo "Build and analyze internal pull request..."
+	mvn clean package sonar:sonar -B -e -V \
 		-Dsonar.host.url=$SONAR_HOST_URL \
 		-Dsonar.login=$SONAR_TOKEN \
 		-Dsonar.analysis.mode=preview \
 		-Dsonar.github.oauth=$GITHUB_TOKEN \
 		-Dsonar.github.repository=$TRAVIS_REPO_SLUG \
 		-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
+else
+	echo 'Build feature branch or external pull request'
+
+    mvn clean -e package
 fi
 # When neither on master branch nor on a non-external pull request => nothing to do
