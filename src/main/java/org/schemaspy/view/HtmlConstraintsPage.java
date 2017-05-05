225854bfcdf3200cf0ec9423977526b7081aea5a
Thomas Traude <T-Riggs@gmx.net>
2017-05-05 07:16:20 +0200

    Remove some dead code, mainly in Html generation classes
    I found and removed those unused methods via my IDE (IntelliJ). So it
    should be safe. Could not find any bugs or missing things during my
    tests afterwards.
    If there was unused code that was considered to be used in the future,
    please let me know.

--- src/main/java/org/schemaspy/view/HtmlConstraintsPage.java
+++ src/main/java/org/schemaspy/view/HtmlConstraintsPage.java
@@ -18,17 +18,16 @@
  */
 package org.schemaspy.view;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
-
-import org.schemaspy.DbAnalyzer;
-import org.schemaspy.model.TableColumn;
-import org.schemaspy.util.HtmlEncoder;
-import org.schemaspy.util.LineWriter;
 import org.schemaspy.model.Database;
 import org.schemaspy.model.ForeignKeyConstraint;
 import org.schemaspy.model.Table;
+import org.schemaspy.util.LineWriter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * The page that lists all of the constraints in the schema
@@ -37,7 +36,6 @@ import org.schemaspy.model.Table;
  */
 public class HtmlConstraintsPage extends HtmlFormatter {
     private static HtmlConstraintsPage instance = new HtmlConstraintsPage();
-    private int columnCounter;
 
     /**
      * Singleton: Don't allow instantiation
@@ -63,186 +61,10 @@ public class HtmlConstraintsPage extends HtmlFormatter {
         mw.write("constraint.html", "constraints.html", "constraint.js");
     }
 
-    private void writeHeader(Database database, LineWriter html) throws IOException {
-        writeHeader(database, null, "Constraints", html);
-        html.writeln("<div class='indent'>");
-    }
-
     @Override
     protected void writeFooter(LineWriter html) throws IOException {
         html.writeln("</div>");
         super.writeFooter(html);
-    }
-
-    /**
-     * Write specified foreign key constraints
-     *
-     * @param constraints List
-     * @param html LineWriter
-     * @throws IOException
-     */
-    private void writeForeignKeyConstraints(List<ForeignKeyConstraint> constraints, LineWriter html) throws IOException {
-        Set<ForeignKeyConstraint> constraintsByName = new TreeSet<ForeignKeyConstraint>();
-        constraintsByName.addAll(constraints);
-
-        html.writeln("<table width='100%'>");
-        html.writeln("<tr><td class='container' valign='bottom'><b>");
-        html.write(String.valueOf(constraintsByName.size()));
-        html.writeln(" Foreign Key Constraints:</b>");
-        html.writeln("</td><td class='container' align='right'>");
-        if (sourceForgeLogoEnabled())
-            html.writeln("  <a href='http://sourceforge.net' target='_blank'><img src='http://sourceforge.net/sflogo.php?group_id=137197&amp;type=1' alt='SourceForge.net' border='0' height='31' width='88'></a>");
-        html.writeln("</td></tr>");
-        html.writeln("</table><br>");
-        html.writeln("<table class='dataTable' border='1' rules='groups'>");
-        html.writeln("<colgroup>");
-        html.writeln("<colgroup>");
-        html.writeln("<colgroup>");
-        html.writeln("<colgroup>");
-        html.writeln("<thead align='left'>");
-        html.writeln("<tr>");
-        html.writeln("  <th>Constraint Name</th>");
-        html.writeln("  <th>Child Column</th>");
-        html.writeln("  <th>Parent Column</th>");
-        html.writeln("  <th>Delete Rule</th>");
-        html.writeln("</tr>");
-        html.writeln("</thead>");
-        html.writeln("<tbody>");
-        for (ForeignKeyConstraint constraint : constraintsByName) {
-            writeForeignKeyConstraint(constraint, html);
-        }
-        if (constraints.size() == 0) {
-            html.writeln(" <tr>");
-            html.writeln("  <td class='detail' valign='top' colspan='4'>None detected</td>");
-            html.writeln(" </tr>");
-        }
-        html.writeln("</tbody>");
-        html.writeln("</table>");
-    }
-
-    /**
-     * Write specified foreign key constraint
-     *
-     * @param constraint ForeignKeyConstraint
-     * @param html LineWriter
-     * @throws IOException
-     */
-    private void writeForeignKeyConstraint(ForeignKeyConstraint constraint, LineWriter html) throws IOException {
-        boolean even = columnCounter++ % 2 == 0;
-        if (even)
-            html.writeln("  <tr class='even'>");
-        else
-            html.writeln("  <tr class='odd'>");
-        html.write("  <td class='detail'>");
-        html.write(constraint.getName());
-        html.writeln("</td>");
-        html.write("  <td class='detail'>");
-        for (Iterator<TableColumn> iter = constraint.getChildColumns().iterator(); iter.hasNext(); ) {
-            TableColumn column = iter.next();
-            html.write("<a href='tables/");
-            html.write(urlEncode(column.getTable().getName()));
-            html.write(".html'>");
-            html.write(column.getTable().getName());
-            html.write("</a>");
-            html.write(".");
-            html.write(column.getName());
-            if (iter.hasNext())
-                html.write("<br>");
-        }
-        html.writeln("</td>");
-        html.write("  <td class='detail'>");
-        for (Iterator<TableColumn> iter = constraint.getParentColumns().iterator(); iter.hasNext(); ) {
-            TableColumn column = iter.next();
-            html.write("<a href='tables/");
-            html.write(urlEncode(column.getTable().getName()));
-            html.write(".html'>");
-            html.write(column.getTable().getName());
-            html.write("</a>");
-            html.write(".");
-            html.write(column.getName());
-            if (iter.hasNext())
-                html.write("<br>");
-        }
-        html.writeln("</td>");
-        html.write("  <td class='detail'>");
-        String ruleText = constraint.getDeleteRuleDescription();
-        String ruleName = constraint.getDeleteRuleName();
-        html.write("<span title='" + ruleText + "'>" + ruleName + "&nbsp;</span>");
-        html.writeln("</td>");
-        html.writeln(" </tr>");
-    }
-
-    /**
-     * Write check constraints associated with the specified tables
-     *
-     * @param tables Collection
-     * @param html LineWriter
-     * @throws IOException
-     */
-    public void writeCheckConstraints(Collection<Table> tables, LineWriter html) throws IOException {
-        html.writeln("<a name='checkConstraints'></a><p>");
-        html.writeln("<b>Check Constraints:</b>");
-        html.writeln("<TABLE class='dataTable' border='1' rules='groups'>");
-        html.writeln("<colgroup>");
-        html.writeln("<colgroup>");
-        html.writeln("<colgroup>");
-        html.writeln("<thead align='left'>");
-        html.writeln("<tr>");
-        html.writeln("  <th>Table</th>");
-        html.writeln("  <th>Constraint Name</th>");
-        html.writeln("  <th>Constraint</th>");
-        html.writeln("</tr>");
-        html.writeln("</thead>");
-        html.writeln("<tbody>");
-
-        List<Table> tablesByName = DbAnalyzer.sortTablesByName(new ArrayList<Table>(tables));
-
-        int constraintsWritten = 0;
-
-        // iter over all tables...only ones with check constraints will write anything
-        for (Table table : tablesByName) {
-            constraintsWritten += writeCheckConstraints(table, html);
-        }
-
-        if (constraintsWritten == 0) {
-            html.writeln(" <tr>");
-            html.writeln("  <td class='detail' valign='top' colspan='3'>None detected</td>");
-            html.writeln(" </tr>");
-        }
-
-        html.writeln("</tbody>");
-        html.writeln("</table>");
-    }
-
-    /**
-     * Write check constraints associated with the specified table (if any)
-     *
-     * @param table Table
-     * @param html LineWriter
-     * @throws IOException
-     * @return int
-     */
-    private int writeCheckConstraints(Table table, LineWriter html) throws IOException {
-        Map<String, String> constraints = table.getCheckConstraints();  // constraint name -> text pairs
-        int constraintsWritten = 0;
-        for (String name : constraints.keySet()) {
-            html.writeln(" <tr>");
-            html.write("  <td class='detail' valign='top'><a href='tables/");
-            html.write(urlEncode(table.getName()));
-            html.write(".html'>");
-            html.write(table.getName());
-            html.write("</a></td>");
-            html.write("  <td class='detail' valign='top'>");
-            html.write(name);
-            html.writeln("</td>");
-            html.write("  <td class='detail'>");
-            html.write(HtmlEncoder.encodeString(constraints.get(name).toString()));
-            html.writeln("</td>");
-            html.writeln(" </tr>");
-            ++constraintsWritten;
-        }
-
-        return constraintsWritten;
     }
 
     @Override
