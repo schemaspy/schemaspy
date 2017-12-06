604535d8663f321c9133de8c199b89b162e291df
Nils Petzaell <nils.petzall@gmail.com>
2017-12-06 12:25:32 +0100

    More relaxed handling on missing Database/ResultSetMetadata
    * We have an SQL92 keywords list, so we don't need to fail if we can't
      get from database
    * If we can't retrieve column from table model using ResulSetMetaData
      check if it contains table/fulltable name in the MetaData and strip it.
      If it's still not found throw an InconsistencyException (this is for
      when DatabaseMetaData returns columns in one way and ResultSetMetaData
      in another.
    * Added basic hive dbtype connectionSpec/connectionString has a bunch of
      options: https://docs.hortonworks.com/HDPDocuments/HDP2/HDP-2.3.0/bk_dataintegration/content/hive-jdbc-odbc-drivers.html

---
+++ src/main/java/org/schemaspy/service/InconsistencyException.java
@@ -0,0 +1,7 @@
+package org.schemaspy.service;
+
+public class InconsistencyException extends RuntimeException {
+    public InconsistencyException(String message) {
+        super(message);
+    }
+}
