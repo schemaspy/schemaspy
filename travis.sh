2250fbed739812e594fa2d8c6b4fc26a4dd03e03
Nils Petzäll <nils.petzall@gmail.com>
2018-01-18 16:00:32 +0100

    Typo

--- travis.sh
+++ travis.sh
@@ -25,7 +25,7 @@ elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
 	# http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
 	# That's why the analysis does not need to be executed if the variable GITHUB_TOKEN is not defined.
 	echo "Build and analyze internal pull request..."
-	mvn -P sonar clean verifygit  -e -V \
+	mvn -P sonar clean verify  -e -V \
 		-Dsonar.host.url=$SONAR_HOST_URL \
 		-Dsonar.login=$SONAR_TOKEN \
 		-Dsonar.analysis.mode=preview \
