a5661e569870de090e67a4d6d7d816e7fa293c5f
Nils Petzaell <nils.petzall@gmail.com>
2018-01-18 22:21:41 +0100

    Add validation that resource found in PropertiesFinder is a file
    * Removed the possibility of reading files without extension.
      We just add .properties if it's missing. Update docs.
    * Fixed a wording issue in schemameta docs.

--- src/main/java/org/schemaspy/db/config/PropertiesFinder.java
+++ src/main/java/org/schemaspy/db/config/PropertiesFinder.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -21,24 +22,15 @@ public class PropertiesFinder implements ResourceFinder {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private static final String DB_TYPES_LOCATION = "org/schemaspy/types/";
-    private static final String DOT_PROPERTIES = ".properties";
 
-    public URL find(String dbType) {
-        URL url = findFile(dbType);
+    public URL find(final String dbType) {
+        String dbTypeToFind = addExtensionIfMissing(dbType);
+        URL url = findFile(dbTypeToFind);
         if (Objects.isNull(url)) {
-            url = findFile(dbType + DOT_PROPERTIES);
+            url = findClassPath(dbTypeToFind);
         }
         if (Objects.isNull(url)) {
-            url = findClassPath(dbType);
-        }
-        if (Objects.isNull(url)) {
-            url = findClassPath(dbType + DOT_PROPERTIES);
-        }
-        if (Objects.isNull(url)) {
-            url = findClassPath(DB_TYPES_LOCATION + dbType);
-        }
-        if (Objects.isNull(url)) {
-            url = findClassPath(DB_TYPES_LOCATION + dbType + DOT_PROPERTIES);
+            url = findClassPath(DB_TYPES_LOCATION + dbTypeToFind);
         }
         if (Objects.isNull(url)) {
             throw new ResourceNotFoundException(dbType);
@@ -46,9 +38,13 @@ public class PropertiesFinder implements ResourceFinder {
         return url;
     }
 
+    private String addExtensionIfMissing(String dbType) {
+        return dbType.toLowerCase().endsWith(".properties") ? dbType : dbType + ".properties";
+    }
+
     private URL findFile(String file) {
         Path path = Paths.get(file);
-        if (path.toFile().exists()) {
+        if (path.toFile().exists() && path.toFile().isFile()) {
             try {
                 return path.toUri().toURL();
             } catch (MalformedURLException e) {
@@ -60,6 +56,15 @@ public class PropertiesFinder implements ResourceFinder {
     }
 
     private URL findClassPath(String resource) {
-        return this.getClass().getClassLoader().getResource(resource);
+        URL url = this.getClass().getClassLoader().getResource(resource);
+        try {
+            if (Objects.nonNull(url) && Paths.get(url.toURI()).toFile().isFile()) {
+                return url;
+            }
+        } catch (URISyntaxException e) {
+            LOGGER.debug("Couldn't convert url to uri to file: {}", url, e);
+            return null;
+        }
+        return null;
     }
 }
