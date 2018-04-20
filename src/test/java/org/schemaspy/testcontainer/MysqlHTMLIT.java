cf2a861ef63169c6567b4e9a675becbdcc153237
Nils Petzaell <nils.petzall@gmail.com>
2018-04-20 21:24:17 +0200

    Ignore database version in MysqlHTMLIT verifyXMLas we do in MysqlXMLIT

--- src/test/java/org/schemaspy/testcontainer/MysqlHTMLIT.java
+++ src/test/java/org/schemaspy/testcontainer/MysqlHTMLIT.java
@@ -7,11 +7,13 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.schemaspy.Main;
+import org.schemaspy.testing.IgnoreUsingXPath;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testcontainers.containers.MySQLContainer;
 import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.builder.Input;
 import org.xmlunit.diff.Diff;
+import org.xmlunit.diff.DifferenceEvaluators;
 
 import java.io.IOException;
 import java.net.URL;
@@ -62,6 +64,7 @@ public class MysqlHTMLIT {
     public void verifyXML() {
         Diff d = DiffBuilder.compare(Input.fromURL(expectedXML))
                 .withTest(Input.fromFile("target/mysqlhtml/test.test.xml"))
+                .withDifferenceEvaluator(DifferenceEvaluators.chain(DifferenceEvaluators.Default, new IgnoreUsingXPath("/database[1]/@type")))
                 .build();
         assertThat(d.getDifferences()).isEmpty();
     }
