4fa1c273d83126c34c98825c8b47de831b9db7b8
Anatoli Babenia <anatoli@rainforce.org>
2019-12-12 14:00:13 +0300

    Fix compatibility problems with /bin/sh

--- docker/schemaspy.sh
+++ docker/schemaspy.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-function run_schemaspy() {
+run_schemaspy() {
     [ -d $SCHEMASPY_DRIVERS ] && export DRIVER_PATH=$SCHEMASPY_DRIVERS || export DRIVER_PATH=/drivers_inc/
     echo -n "Using drivers:"
     ls -Ax $DRIVER_PATH | sed -e 's/  */, /g'
     exec java -jar /usr/local/lib/schemaspy/schemaspy*.jar -dp $DRIVER_PATH -o $SCHEMASPY_OUTPUT "$@"
 }
 
+echo "Entrypoint script params:"
+echo "$@"
 # If the first argument to entrypoint starts with dash
-if [[ $1 == -* ]]; then
-	exec run_schemaspy
+if echo "$1" | grep -qE '^-'
+then
+	run_schemaspy
 else
 	exec "$@"
 fi
