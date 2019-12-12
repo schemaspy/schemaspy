4264acaa366444169b9209abf64a6c48500142da
Anatoli Babenia <anatoli@rainforce.org>
2019-12-12 11:57:45 +0300

    Allow to run other commands in in container
    If the first argument passed to entrypoint doesn't start
    with dash, it is the command that needs to be executed.
    
    Taken from the best practices on ENTRYPOINT
    https://docs.docker.com/develop/develop-images/dockerfile_best-practices/#entrypoint

--- docker/schemaspy.sh
+++ docker/schemaspy.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-[ -d $SCHEMASPY_DRIVERS ] && export DRIVER_PATH=$SCHEMASPY_DRIVERS || export DRIVER_PATH=/drivers_inc/
-echo -n "Using drivers:"
-ls -Ax $DRIVER_PATH | sed -e 's/  */, /g'
-exec java -jar /usr/local/lib/schemaspy/schemaspy*.jar -dp $DRIVER_PATH -o $SCHEMASPY_OUTPUT "$@"
+
+function run_schemaspy() {
+    [ -d $SCHEMASPY_DRIVERS ] && export DRIVER_PATH=$SCHEMASPY_DRIVERS || export DRIVER_PATH=/drivers_inc/
+    echo -n "Using drivers:"
+    ls -Ax $DRIVER_PATH | sed -e 's/  */, /g'
+    exec java -jar /usr/local/lib/schemaspy/schemaspy*.jar -dp $DRIVER_PATH -o $SCHEMASPY_OUTPUT "$@"
+}
+
+# If the first argument to entrypoint starts with dash
+if [[ $1 == -* ]]; then
+	exec run_schemaspy
+else
+	exec "$@"
+fi
