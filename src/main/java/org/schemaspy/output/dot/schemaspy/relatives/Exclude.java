69a9c2f2e1957feb3103ff8834cfca9ccbae48a2
Jesper Olsson <software@jesperolsson.se>
2021-06-22 07:16:55 +0200

    refactor(dot): Encapsulate exclusion algorithm

---
+++ src/main/java/org/schemaspy/output/dot/schemaspy/relatives/Exclude.java
@@ -0,0 +1,59 @@
+package org.schemaspy.output.dot.schemaspy.relatives;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.schemaspy.model.Table;
+import org.schemaspy.model.TableColumn;
+
+public final class Exclude {
+
+    private final Table table;
+
+    public Exclude(final Table table) {
+        this.table = table;
+    }
+
+    public Iterable<Verdict> children() {
+        Set<Verdict> result = new HashSet<>();
+        for (TableColumn column : table.getColumns()) {
+            if (column.isAllExcluded()) {
+                continue;
+            }
+            if (column.isExcluded()) {
+                continue;
+            }
+            for (TableColumn childColumn : column.getChildren()) {
+                if (childColumn.isAllExcluded()) {
+                    continue;
+                }
+                if(childColumn.isExcluded()) {
+                    continue;
+                }
+                result.add(new Verdict(column, childColumn));
+            }
+        }
+        return result;
+    }
+
+    public Iterable<Verdict> parents() {
+        Set<Verdict> result = new HashSet<>();
+        for (TableColumn column : table.getColumns()) {
+            if (column.isAllExcluded()) {
+                continue;
+            }
+            if (column.isExcluded()) {
+                continue;
+            }
+            for (TableColumn parentColumn : column.getParents()) {
+                if (parentColumn.isAllExcluded()) {
+                    continue;
+                }
+                if (parentColumn.isExcluded()) {
+                    continue;
+                }
+                result.add(new Verdict(column, parentColumn));
+            }
+        }
+        return result;
+    }
+}
