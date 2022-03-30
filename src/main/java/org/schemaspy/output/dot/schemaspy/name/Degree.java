1b810f779ac340ab94cffc439f1f4fde1c71d282
Jesper Olsson <software@jesperolsson.se>
2022-03-30 07:29:32 +0200

    refactor(dot): Remove origin from degree

--- src/main/java/org/schemaspy/output/dot/schemaspy/name/Degree.java
+++ src/main/java/org/schemaspy/output/dot/schemaspy/name/Degree.java
@@ -1,27 +1,24 @@
 package org.schemaspy.output.dot.schemaspy.name;
 
 /**
- * Decorates a name based on number of degrees of separation.
+ * Encapsulates what name to use based on number of degrees of separation.
  */
 public final class Degree implements Name {
 
     private final boolean twoDegreesOfSeparation;
-    private final Name origin;
 
     /**
      * Constructor.
      *
      * @param twoDegreesOfSeparation True if the degrees of separation is two,
            false if that number is one.
-     * @param origin The name to be decorated.
      */
-    public Degree(final boolean twoDegreesOfSeparation, final Name origin) {
+    public Degree(final boolean twoDegreesOfSeparation) {
         this.twoDegreesOfSeparation = twoDegreesOfSeparation;
-        this.origin = origin;
     }
 
     @Override
     public String value() {
-        return (twoDegreesOfSeparation ? "twoDegrees" : "oneDegree") + origin.value();
+        return twoDegreesOfSeparation ? "twoDegrees" : "oneDegree";
     }
 }
