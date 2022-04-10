726cb5db5385b004122ac1a91033080f058dc735
Jesper Olsson <software@jesperolsson.se>
2022-04-10 17:05:28 +0200

    refactor(dot): Remove origin from sized

--- src/main/java/org/schemaspy/output/dot/schemaspy/name/Sized.java
+++ src/main/java/org/schemaspy/output/dot/schemaspy/name/Sized.java
@@ -1,26 +1,23 @@
 package org.schemaspy.output.dot.schemaspy.name;
 
 /**
- * Decorates a name based on compactness.
+ * Encapsulates a name based on compactness.
  */
 public final class Sized implements Name {
 
     private final boolean isCompact;
-    private final Name origin;
 
     /**
      * Constructor.
      *
      * @param isCompact True if the graph is compact, false if large.
-     * @param origin The name to be decorated.
      */
-    public Sized(final boolean isCompact, final Name origin) {
+    public Sized(final boolean isCompact) {
         this.isCompact = isCompact;
-        this.origin = origin;
     }
 
     @Override
     public String value() {
-        return (isCompact ? "compact" : "large") + origin.value();
+        return this.isCompact ? "compact" : "large";
     }
 }
