0910368e96a9f7541d69e7a14d925861610ab794
Jesper Olsson <software@jesperolsson.se>
2022-05-12 21:03:55 +0200

    refactor(dot): Create digraph

---
+++ src/main/java/org/schemaspy/output/dot/schemaspy/graph/Digraph.java
@@ -0,0 +1,32 @@
+package org.schemaspy.output.dot.schemaspy.graph;
+
+import org.schemaspy.output.dot.schemaspy.Header;
+import org.schemaspy.output.dot.schemaspy.name.Name;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public final class Digraph implements Graph {
+
+    private final Name name;
+    private final Header header;
+    private final Element[] contents;
+
+    public Digraph(final Name name, final Header header, final Element... contents) {
+        this.name = name;
+        this.header = header;
+        this.contents = contents;
+    }
+
+    @Override
+    public String dot() {
+        return String.format(
+                "digraph \"%s\" { %s %s }",
+                this.name.value(),
+                this.header.value(),
+                Arrays.stream(this.contents)
+                        .map(Element::value)
+                        .collect(Collectors.joining(" "))
+        );
+    }
+}
---
+++ src/test/java/org/schemaspy/output/dot/schemaspy/graph/DigraphTest.java
@@ -0,0 +1,82 @@
+package org.schemaspy.output.dot.schemaspy.graph;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link Digraph}.
+ */
+public class DigraphTest {
+
+    /**
+     * When the object is asked to represent itself in DOT,
+     * Then it should include its name.
+     */
+    @Test
+    void writeName() {
+        final String key = "Graph";
+        MatcherAssert.assertThat(
+                new Digraph(
+                        () -> key,
+                        () -> "",
+                        () -> ""
+                ).dot(),
+                CoreMatchers.containsString("digraph \"" + key + "\" {")
+        );
+    }
+
+    /**
+     * When the object is asked to represent itself in DOT,
+     * Then it should include its header.
+     */
+    @Test
+    void writeHeader() {
+        final String key = "header";
+        MatcherAssert.assertThat(
+                new Digraph(
+                        () -> "",
+                        () -> key,
+                        () -> ""
+                ).dot(),
+                CoreMatchers.containsString(key)
+        );
+    }
+
+    /**
+     * Given a lone node,
+     * When the object is asked to represent itself in DOT,
+     * Then it should include that node.
+     */
+    @Test
+    void writeSingleElement() {
+        final String key = "node";
+        MatcherAssert.assertThat(
+                new Digraph(
+                        () -> "",
+                        () -> "",
+                        () -> key
+                ).dot(),
+                CoreMatchers.containsString(key)
+        );
+    }
+
+    /**
+     * Given multiple nodes,
+     * When the object is asked to represent itself in DOT,
+     * Then it should include both nodes.
+     */
+    @Test
+    void writeMultipleElement() {
+        final String key1 = "node1";
+        final String key2 = "node2";
+        final String result = new Digraph(
+                () -> "",
+                () -> "",
+                () -> key1,
+                () -> key2
+        ).dot();
+        MatcherAssert.assertThat(result, CoreMatchers.containsString(key1));
+        MatcherAssert.assertThat(result, CoreMatchers.containsString(key2));
+    }
+}
