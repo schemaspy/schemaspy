3a2b92257deaab02792732317c4e8996f29e866c
Jesper Olsson <software@jesperolsson.se>
2022-06-17 12:50:27 +0200

    refactor(analyzer): Introduce decorator

---
+++ src/main/java/org/schemaspy/model/Console.java
@@ -0,0 +1,108 @@
+package org.schemaspy.model;
+
+import org.schemaspy.Config;
+import org.schemaspy.cli.CommandLineArguments;
+
+import java.io.File;
+import java.util.Collection;
+
+/**
+ * Decorator for rendering console-based details.
+ */
+public class Console implements ProgressListener {
+
+    private final CommandLineArguments commandLineArguments;
+    private final ProgressListener origin;
+
+    public Console(final CommandLineArguments commandLineArguments, final ProgressListener origin) {
+        this.commandLineArguments = commandLineArguments;
+        this.origin = origin;
+    }
+
+    @Override
+    public long startedGatheringDetails() {
+        long result = origin.startedGatheringDetails();
+        System.out.print("Gathering schema details...");
+        return result;
+    }
+
+    @Override
+    public void gatheringDetailsProgressed(Table table) {
+        origin.gatheringDetailsProgressed(table);
+        System.out.print('.');
+    }
+
+    @Override
+    public long startedConnectingTables() {
+        long result = origin.startedConnectingTables();
+        System.err.flush();
+        System.out.flush();
+        System.out.println("(" + result / 1000 + "sec)");
+        System.out.print("Connecting relationships...");
+        return result;
+    }
+
+    @Override
+    public void connectingTablesProgressed(Table table) {
+        origin.connectingTablesProgressed(table);
+        System.out.print('.');
+    }
+
+    @Override
+    public long startedGraphingSummaries() {
+        long result = origin.startedGraphingSummaries();
+        System.err.flush();
+        System.out.flush();
+        System.out.println("(" + result / 1000 + "sec)");
+        System.out.print("Writing/graphing summary");
+        System.out.print('.');
+        return result;
+
+    }
+
+    @Override
+    public void graphingSummaryProgressed() {
+        origin.graphingSummaryProgressed();
+        System.out.print('.');
+    }
+
+    @Override
+    public long startedGraphingDetails() {
+        long result = origin.startedGraphingDetails();
+        System.err.flush();
+        System.out.flush();
+        System.out.println("(" + result / 1000 + "sec)");
+        System.out.print("Writing/diagramming details");
+        return result;
+    }
+
+    @Override
+    public void graphingDetailsProgressed(Table table) {
+        origin.graphingDetailsProgressed(table);
+        System.out.print('.');
+    }
+
+    @Override
+    public long finishedGatheringDetails() {
+        long result = origin.finishedGatheringDetails();
+        System.err.flush();
+        System.out.flush();
+        System.out.println("(" + result / 1000 + "sec)");
+        return result;
+    }
+
+    @Override
+    public long finished(Collection<Table> tables, Config config) {
+        long result = origin.finished(tables, config);
+        System.err.flush();
+        System.out.flush();
+        System.out.println("Wrote relationship details of " + tables.size() + " tables/views to directory '" + commandLineArguments.getOutputDirectory() + "' in " + result / 1000 + " seconds.");
+        System.out.println("View the results by opening " + new File(commandLineArguments.getOutputDirectory(), "index.html"));
+        return result;
+    }
+
+    @Override
+    public String recoverableExceptionEncountered(String msg, Exception exc, String sql) {
+        return origin.recoverableExceptionEncountered(msg, exc, sql);
+    }
+}
---
+++ src/main/java/org/schemaspy/model/Tracked.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2004 - 2011, 2014 John Currier
+ * Copyright (C) 2017 Thomas Traude
+ *
+ * This file is a part of the SchemaSpy project (http://schemaspy.org).
+ *
+ * SchemaSpy is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SchemaSpy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.schemaspy.model;
+
+import org.schemaspy.Config;
+
+import java.util.Collection;
+
+/**
+ * Implementation of {@link ProgressListener} that sends its output to the console.
+ *
+ * @author John Currier
+ * @author Thomas Traude
+ */
+public class Tracked implements ProgressListener {
+
+	private long startedAt;
+	private long startedGatheringAt;
+	private long startedConnectingAt;
+	private long startedGraphingSummariesAt;
+	private long startedGraphingDetailsAt;
+	private long finishedAt;
+
+    public Tracked() {
+        startedAt = System.currentTimeMillis();
+    }
+
+	@Override
+	public long startedGatheringDetails() {
+        startedGatheringAt = System.currentTimeMillis();
+        return startedAt - startedGatheringAt;
+	}
+
+	@Override
+	public void gatheringDetailsProgressed(Table table) { }
+
+	@Override
+	public long startedConnectingTables() {
+        startedConnectingAt = System.currentTimeMillis();
+        return startedConnectingAt - startedGatheringAt;
+	}
+
+	@Override
+	public void connectingTablesProgressed(Table table) { }
+
+	@Override
+	public long startedGraphingSummaries() {
+        startedGraphingSummariesAt = System.currentTimeMillis();
+        return startedGraphingSummariesAt - startedConnectingAt;
+	}
+
+	@Override
+	public void graphingSummaryProgressed() { }
+
+	@Override
+	public long startedGraphingDetails() {
+		startedGraphingDetailsAt = System.currentTimeMillis();
+		return startedGraphingDetailsAt - startedGraphingSummariesAt;
+	}
+
+	@Override
+	public void graphingDetailsProgressed(Table table) { }
+
+	@Override
+	public long finishedGatheringDetails() {
+		finishedAt = System.currentTimeMillis();
+		return finishedAt - startedGraphingDetailsAt;
+	}
+
+	@Override
+	public long finished(Collection<Table> tables, Config config) {
+		finishedAt = System.currentTimeMillis();
+		return finishedAt - startedAt;
+	}
+
+	@Override
+	public String recoverableExceptionEncountered(String msg, Exception exc, String sql) {
+        System.out.println();
+        System.out.flush();
+
+        String text = msg + ": " + exc;
+        if (sql != null) {
+        	text += ": " + sql;
+        }
+
+        return text;
+	}
+}
