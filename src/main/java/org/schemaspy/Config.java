7eeb9f97a7d299d8d2bcf967324aaef6cc9c94c0
Jesper Olsson <software@jesperolsson.se>
2022-08-08 16:01:21 +0200

    refactor(con): Extract class

--- src/main/java/org/schemaspy/Config.java
+++ src/main/java/org/schemaspy/Config.java
@@ -99,7 +99,6 @@ public final class Config implements HtmlConfig {
     private Pattern tableExclusions;
     private Pattern columnExclusions;
     private Pattern indirectColumnExclusions;
-    private Properties userConnectionProperties;
     private Integer maxDbThreads;
     private Integer maxDetailedTables;
     private String driverPath;
@@ -364,71 +363,8 @@ public final class Config implements HtmlConfig {
         return maxDetailedTables;
     }
 
-    /**
-     * Properties from this file (in key=value pair format) are passed to the
-     * database connection.<br>
-     * user (from -u) and password (from -p) will be passed in the
-     * connection properties if specified.
-     *
-     * @param propertiesFilename file to use for connection properties
-     * @throws IOException if we have problems reading the file
-     */
-    public void setConnectionPropertiesFile(String propertiesFilename) throws IOException {
-        if (userConnectionProperties == null)
-            userConnectionProperties = new Properties();
-        try (InputStream inputStream = new FileInputStream(propertiesFilename)) {
-            userConnectionProperties.load(inputStream);
-        }
-    }
-
-    /**
-     * Returns a {@link Properties} populated either from the properties file specified
-     * by {@link #setConnectionPropertiesFile(String)}, the properties specified by
-     * {@link #setConnectionProperties(String)} or not populated.
-     *
-     * @return connection properties to use when connecting
-     * @throws IOException if we a have problems reading the properties file if -connprops is a file
-     */
-    public Properties getConnectionProperties() throws IOException {
-        if (userConnectionProperties == null) {
-            String props = pullParam("-connprops");
-            if (props != null) {
-                if (props.contains(ESCAPED_EQUALS)) {
-                    setConnectionProperties(props);
-                } else {
-                    setConnectionPropertiesFile(props);
-                }
-            } else {
-                userConnectionProperties = new Properties();
-            }
-        }
-
-        return userConnectionProperties;
-    }
-
-    /**
-     * Specifies connection properties to use in the format:
-     * <code>key1\=value1;key2\=value2</code><br>
-     * user (from -u) and password (from -p) will be passed in the
-     * connection properties if specified.<p>
-     * This is an alternative form of passing connection properties than by file
-     * (see {@link #setConnectionPropertiesFile(String)})
-     *
-     * @param properties string with key\\=value pairs separated by ; of connection properties
-     */
-    public void setConnectionProperties(String properties) {
-        userConnectionProperties = new Properties();
-
-        StringTokenizer tokenizer = new StringTokenizer(properties, ";");
-        while (tokenizer.hasMoreElements()) {
-            String pair = tokenizer.nextToken();
-            int index = pair.indexOf(ESCAPED_EQUALS);
-            if (index != -1) {
-                String key = pair.substring(0, index);
-                String value = pair.substring(index + ESCAPED_EQUALS.length());
-                userConnectionProperties.put(key, value);
-            }
-        }
+    public String connprops() {
+        return pullParam("-connprops");
     }
 
     public void setDriverPath(String driverPath) {
