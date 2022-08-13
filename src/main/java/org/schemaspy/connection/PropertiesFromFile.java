bc49d52d5ff2bbfbb818df9165bab6d6fbe75328
Jesper Olsson <software@jesperolsson.se>
2022-08-13 09:11:28 +0200

    refactor(con): Extract class

---
+++ src/main/java/org/schemaspy/connection/PropertiesFromFile.java
@@ -0,0 +1,35 @@
+package org.schemaspy.connection;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Properties from this file (in key=value pair format) are passed to the
+ * database connection.<br>
+ * user (from -u) and password (from -p) will be passed in the
+ * connection properties if specified.
+ */
+public class PropertiesFromFile {
+
+    private final String propertiesFilename;
+
+    /**
+     * @param propertiesFilename file to use for connection properties
+     */
+    public PropertiesFromFile(String propertiesFilename) {
+        this.propertiesFilename = propertiesFilename;
+    }
+
+    /**
+     * @throws IOException if we have problems reading the file
+     */
+    public Properties setConnectionPropertiesFile() throws IOException {
+        Properties result = new Properties();
+        try (InputStream inputStream = new FileInputStream(propertiesFilename)) {
+            result.load(inputStream);
+        }
+        return result;
+    }
+}
