b03d8e7da9fd89cf1c4b1a8daa235407bd5747d2
Carl J. Mosca <carljmosca@gmail.com>
2022-08-27 15:18:53 -0400

    renamed env variable

--- docker/schemaspy.sh
+++ docker/schemaspy.sh
@@ -3,7 +3,7 @@
 echo -n "Using drivers:"
 ls -Ax $DRIVER_PATH | sed -e 's/  */, /g'
 exec java -jar /usr/local/lib/schemaspy/schemaspy*.jar \
-  -t $DB_TYPE -dp $DRIVER_PATH -db $DB_NAME \
+  -t $DB_TYPE -dp $DRIVER_PATH -db $SQL_DATABASE \
   -host $SQL_HOST -port $SQL_PORT \
   -u $SQL_USER -p $SQL_PASSWORD \
   -o $SCHEMASPY_OUTPUT
