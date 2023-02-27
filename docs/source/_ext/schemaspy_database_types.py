b51d2a64aac4cf2fb533b30cd75ec7abb780af8f
Nils Petzall <nils.petzall@gmail.com>
2023-02-28 00:33:10 +0100

    chore(docs): update database_types extension to use flat-table

--- docs/source/_ext/schemaspy_database_types.py
+++ docs/source/_ext/schemaspy_database_types.py
@@ -1,5 +1,6 @@
 import os
 from os.path import join
+from collections import defaultdict
 
 from docutils.frontend import OptionParser
 from docutils.utils import new_document
@@ -10,7 +11,7 @@ from sphinx.util.docutils import SphinxDirective
 class SchemaSpyDatabaseTypes(SphinxDirective):
 
     def run(self):
-        db_types = dict()
+        per_dbms = defaultdict(list)
         with os.scandir(join('..', 'src', 'main', 'resources', 'org', 'schemaspy', 'types')) as i:
             for entry in i:
                 if entry.is_file():
@@ -24,12 +25,25 @@ class SchemaSpyDatabaseTypes(SphinxDirective):
                         else:
                             lines_merged.append(line)
                         append = line[-1] == "\\"
-                    db_types[entry.name[0:-11]] = {x[0]: x[1] for x in [y.split("=", 1) for y in lines_merged] if len(x) == 2}
+                    db_type = {x[0].strip(): x[1].strip() for x in [y.split("=", 1) for y in lines_merged] if len(x) == 2}
+                    db_type['arg'] = entry.name[0:-11]
+                    per_dbms[db_type.get("dbms")].append(db_type)
         rst_text = ""
-        for (k,v) in db_types.items():
-            rst_text += "| **" + v.get('description', 'missing').strip() + "**\n"
-            rst_text += "| \t ``-t " + k + "``\n\n"
-        return self.parse_rst(rst_text)
+        rst_text += ".. flat-table:: \n"
+        rst_text += "   :class: clean-table\n"
+        rst_text += "   :header-rows: 1\n\n"
+        rst_text += "   * - Dbms\n"
+        rst_text += "     - Description\n"
+        rst_text += "     - Argument `-t`\n"
+        for key in sorted(per_dbms):
+            rst_text += "   * - :rspan:`" + str(len(per_dbms[key])-1) + "` "+ key +"\n"
+            for i, v in enumerate(per_dbms[key]):
+                if i == 0:
+                    rst_text += "     - " + v.get('description') + "\n"
+                else:
+                    rst_text += "   * - " + v.get('description') + "\n"
+                rst_text += "     - " + v.get('arg') + "\n"
+        return self.parse_rst(rst_text + "\n\n")
 
     def parse_rst(self, text):
         parser = RSTParser()
