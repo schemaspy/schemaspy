13c3f3fcb3515d48232b4991b207508594c61f73
Nils Petzall <nils.petzall@gmail.com>
2023-03-04 00:40:23 +0100

    chore(docs): sphinx extension for databaseTypes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,10 +16,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
+
+sys.path.append(os.path.abspath("./_ext"))
 
 
 # -- General configuration ------------------------------------------------
@@ -31,7 +33,7 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx_rtd_theme','linuxdoc.rstFlatTable','schemaspy_database_types']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -64,7 +66,7 @@ release = u'6.0.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -97,6 +99,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ["css/clean_table.css"]
 
 
 # -- Options for HTMLHelp output ------------------------------------------
