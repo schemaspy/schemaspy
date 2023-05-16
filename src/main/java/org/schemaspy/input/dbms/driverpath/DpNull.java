8950e067e7882576254d5062c7a57a9636d68aea
Jesper Olsson <software@jesperolsson.se>
2023-05-16 21:30:11 +0200

    refactor(dp): Null object

---
+++ src/main/java/org/schemaspy/input/dbms/driverpath/DpNull.java
@@ -0,0 +1,12 @@
+package org.schemaspy.input.dbms.driverpath;
+
+/**
+ * Null object for driverpath representation.
+ */
+public final class DpNull implements Driverpath {
+
+    @Override
+    public String value() {
+        return "";
+    }
+}
