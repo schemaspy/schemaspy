ac86808ccd1d0cdb568c1ad1269c28e4e6dc9d6c
Jesper Olsson <software@jesperolsson.se>
2023-05-19 17:01:31 +0200

    refactor(dp): Fallback

---
+++ src/main/java/org/schemaspy/input/dbms/driverpath/DpFallback.java
@@ -0,0 +1,22 @@
+package org.schemaspy.input.dbms.driverpath;
+
+import java.util.Objects;
+
+/**
+ * Uses the fallback driverpath in case the first is unavailable.
+ */
+public final class DpFallback implements Driverpath {
+
+    private final Driverpath primary;
+    private final Driverpath secondary;
+
+    public DpFallback(final Driverpath primary, final Driverpath secondary) {
+        this.primary = primary;
+        this.secondary = secondary;
+    }
+
+    @Override
+    public String value() {
+        return !Objects.isNull(this.primary) ? this.primary.value() : this.secondary.value();
+    }
+}
