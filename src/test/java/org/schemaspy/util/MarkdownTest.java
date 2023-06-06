0613fd74ccfce87e9667f9f2b90c1bc0fe6c6210
Samuel Dussault <samuel.dussault@usherbrooke.ca>
2023-06-06 09:47:43 -0400

    feature(Asciidoc): Markdown unit test should not use the generic class

--- src/test/java/org/schemaspy/util/MarkdownTest.java
+++ src/test/java/org/schemaspy/util/MarkdownTest.java
@@ -1,7 +1,7 @@
 package org.schemaspy.util;
 
 import org.junit.jupiter.api.Test;
-import org.schemaspy.util.markup.MarkupProcessor;
+import org.schemaspy.util.markup.Markdown;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,14 +10,14 @@ class MarkdownTest {
     @Test
     void willNotReplaceDirectNewLineToBR() {
         String sourceMarkdown = "Line\n with\n no\n hardbreak\n";
-        String renderedMarkdown = MarkupProcessor.getInstance().toHtml(sourceMarkdown, ".");
+        String renderedMarkdown = new Markdown().toHtml(sourceMarkdown, ".");
         assertThat(renderedMarkdown).doesNotContain("<br />");
     }
 
     @Test
     void willReplaceNewLineWhenPrecededByTwoSpacesAsBR() {
         String sourceMarkdown = "Line  \n with  \n no  \n hardbreak  \n";
-        String renderedMarkdown = MarkupProcessor.getInstance().toHtml(sourceMarkdown, ".");
+        String renderedMarkdown = new Markdown().toHtml(sourceMarkdown, ".");
         assertThat(renderedMarkdown).contains("<br />");
     }
 }
