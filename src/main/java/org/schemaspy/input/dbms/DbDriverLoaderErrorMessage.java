8065ace99447bcbbc2e9a0f290c6c95dd46e359c
Jesper Olsson <software@jesperolsson.se>
2023-09-15 21:50:24 +0200

    chore(DBdl): Simplify error message

--- src/main/java/org/schemaspy/input/dbms/DbDriverLoaderErrorMessage.java
+++ src/main/java/org/schemaspy/input/dbms/DbDriverLoaderErrorMessage.java
@@ -3,23 +3,18 @@ package org.schemaspy.input.dbms;
 public class DbDriverLoaderErrorMessage {
 
     private final String[] driverClass;
-    private final String driverPath;
 
     public DbDriverLoaderErrorMessage(
-            final String []driverClass,
-            final String driverPath
+            final String []driverClass
     ) {
         this.driverClass = driverClass;
-        this.driverPath = driverPath;
     }
 
     public String createMessage() {
         StringBuilder sb = new StringBuilder()
                 .append("Failed to create any of '")
                 .append(String.join(", ", driverClass))
-                .append("' driver from driverPath '")
-                .append(driverPath)
-                .append(".");
+                .append("' driver from driver path.");
         return sb.toString();
     }
 }
