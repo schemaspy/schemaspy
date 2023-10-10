9ab6a3286ec94b5cb9c2822ed5263f9a0c9f59b4
Jesper Olsson <software@jesperolsson.se>
2023-10-10 21:22:21 +0200

    refactor(ds): Simplify caching

--- src/main/java/org/schemaspy/input/dbms/driver/DsCached.java
+++ src/main/java/org/schemaspy/input/dbms/driver/DsCached.java
@@ -9,18 +9,15 @@ public class DsCached implements Driversource {
 
     private static Map<String, Driver> driverCache = new HashMap<>();
 
-    private final String[] driverClasses;
     private final String driverPath;
     private final Class<Driver> driverClass;
     private final Driversource origin;
 
     public DsCached(
-        final String[] driverClasses,
         final String driverPath,
         final Class<Driver> driverClass,
         final Driversource origin
     ) {
-        this.driverClasses = driverClasses;
         this.driverPath = driverPath;
         this.driverClass = driverClass;
         this.origin = origin;
@@ -28,12 +25,9 @@ public class DsCached implements Driversource {
 
     @Override
     public Driver driver() {
-        Driver driver;
-        for (String driverClass: driverClasses) {
-            driver = driverCache.get(driverClass + "|" + driverPath);
-            if (Objects.nonNull(driver)) {
-                return driver;
-            }
+        Driver driver = driverCache.get(driverClass.getName() + "|" + driverPath);
+        if (Objects.nonNull(driver)) {
+            return driver;
         }
         driver = this.origin.driver();
         driverCache.put(driverClass.getName() + "|" + driverPath, driver);
