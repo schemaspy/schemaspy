1f42edc182c1b35d857e66799f623eafd6ce618e
Jesper Olsson <software@jesperolsson.se>
2023-10-16 18:36:01 +0200

    refactor(SqlCon): Introduce interface

---
+++ src/main/java/org/schemaspy/connection/ScSimple.java
@@ -0,0 +1,64 @@
+package org.schemaspy.connection;
+
+import org.schemaspy.input.dbms.ConnectionConfig;
+import org.schemaspy.input.dbms.ConnectionURLBuilder;
+import org.schemaspy.input.dbms.driver.Driversource;
+import org.schemaspy.input.dbms.exceptions.ConnectionFailure;
+
+import java.io.IOException;
+import java.sql.Driver;
+import java.util.Properties;
+
+public final class ScSimple implements SqlConnection{
+
+    private final Connection con;
+    private final ConnectionURLBuilder urlBuilder;
+    private final Driversource driversource;
+
+    public ScSimple(
+        final ConnectionConfig connectionConfig,
+        final ConnectionURLBuilder urlBuilder,
+        final Driversource driversource
+    ) {
+        this(
+            new WithPassword(
+                connectionConfig.getPassword(),
+                new WithUser(
+                    connectionConfig.getUser(),
+                    new PreferencesConnection(connectionConfig.getConnectionProperties())
+                )
+            ),
+            urlBuilder,
+            driversource
+        );
+    }
+
+    public ScSimple(
+        final Connection con,
+        final ConnectionURLBuilder urlBuilder,
+        final Driversource driversource
+    ) {
+        this.con = con;
+        this.urlBuilder = urlBuilder;
+        this.driversource = driversource;
+    }
+
+    @Override
+    public java.sql.Connection connection() throws IOException {
+        String connectionURL = urlBuilder.build();
+
+        final Properties connectionProperties = this.con.properties();
+
+        java.sql.Connection connection;
+        try {
+            Driver driver = driversource.driver();
+            connection = driver.connect(connectionURL, connectionProperties);
+            if (connection == null) {
+                throw new ConnectionFailure("Cannot connect to '" + connectionURL + "'");
+            }
+        } catch (Exception exc) {
+            throw new ConnectionFailure("Failed to connect to database URL [" + connectionURL + "]", exc);
+        }
+        return connection;
+    }
+}
--- src/main/java/org/schemaspy/connection/SqlConnection.java
+++ src/main/java/org/schemaspy/connection/SqlConnection.java
@@ -1,63 +1,16 @@
 package org.schemaspy.connection;
 
-import org.schemaspy.input.dbms.ConnectionConfig;
-import org.schemaspy.input.dbms.ConnectionURLBuilder;
-import org.schemaspy.input.dbms.driver.Driversource;
-import org.schemaspy.input.dbms.exceptions.ConnectionFailure;
-
 import java.io.IOException;
-import java.sql.Driver;
-import java.util.Properties;
 
-public final class SqlConnection {
+/**
+ * Adapter to the standard library for SQL connections.
+ */
+public interface SqlConnection {
 
-    private final Connection con;
-    private final ConnectionURLBuilder urlBuilder;
-    private final Driversource driversource;
-
-    public SqlConnection(
-        final ConnectionConfig connectionConfig,
-        final ConnectionURLBuilder urlBuilder,
-        final Driversource driversource
-    ) {
-        this(
-            new WithPassword(
-                connectionConfig.getPassword(),
-                new WithUser(
-                    connectionConfig.getUser(),
-                    new PreferencesConnection(connectionConfig.getConnectionProperties())
-                )
-            ),
-            urlBuilder,
-            driversource
-        );
-    }
-
-    public SqlConnection(
-        final Connection con,
-        final ConnectionURLBuilder urlBuilder,
-        final Driversource driversource
-    ) {
-        this.con = con;
-        this.urlBuilder = urlBuilder;
-        this.driversource = driversource;
-    }
-
-    public java.sql.Connection connection() throws IOException {
-        String connectionURL = urlBuilder.build();
-
-        final Properties connectionProperties = this.con.properties();
-
-        java.sql.Connection connection;
-        try {
-            Driver driver = driversource.driver();
-            connection = driver.connect(connectionURL, connectionProperties);
-            if (connection == null) {
-                throw new ConnectionFailure("Cannot connect to '" + connectionURL + "'");
-            }
-        } catch (Exception exc) {
-            throw new ConnectionFailure("Failed to connect to database URL [" + connectionURL + "]", exc);
-        }
-        return connection;
-    }
+    /**
+     * Asks the connection to interface itself as the standard library.
+     * @return The SQL connection as a standard library interface.
+     * @throws IOException If the connection is erroneous.
+     */
+    java.sql.Connection connection() throws IOException;
 }
