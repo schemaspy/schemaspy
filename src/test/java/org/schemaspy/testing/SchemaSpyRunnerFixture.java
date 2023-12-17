b5ac5ab986d5b88b67e0a83f856fb4e5fe3d2d15
Jesper Olsson <software@jesperolsson.se>
2023-12-17 18:10:35 +0100

    refactor(sa): inject sql connection

--- src/test/java/org/schemaspy/testing/SchemaSpyRunnerFixture.java
+++ src/test/java/org/schemaspy/testing/SchemaSpyRunnerFixture.java
@@ -3,6 +3,13 @@ package org.schemaspy.testing;
 import org.schemaspy.LayoutFolder;
 import org.schemaspy.SchemaAnalyzer;
 import org.schemaspy.cli.*;
+import org.schemaspy.connection.ScExceptionChecked;
+import org.schemaspy.connection.ScNullChecked;
+import org.schemaspy.connection.ScSimple;
+import org.schemaspy.connection.SqlConnection;
+import org.schemaspy.input.dbms.ConnectionConfig;
+import org.schemaspy.input.dbms.ConnectionURLBuilder;
+import org.schemaspy.input.dbms.DriverFromConfig;
 import org.schemaspy.input.dbms.service.DatabaseServiceFactory;
 import org.schemaspy.input.dbms.service.SqlService;
 import org.schemaspy.output.xml.dom.XmlProducerUsingDOM;
@@ -21,16 +28,29 @@ public class SchemaSpyRunnerFixture {
                         args
                 ).commandLineArguments();
         SqlService sqlService = new SqlService();
+        final ConnectionConfig connectionConfig = commandLineArguments.getConnectionConfig();
+        final ConnectionURLBuilder urlBuilder = new ConnectionURLBuilder(connectionConfig);
         return new SchemaSpyRunner(
-                new SchemaAnalyzer(
-                        sqlService,
-                        new DatabaseServiceFactory(sqlService),
-                        commandLineArguments,
-                        new XmlProducerUsingDOM(),
-                        new LayoutFolder(SchemaAnalyzer.class.getClassLoader())
-                ),
+            new SchemaAnalyzer(
+                sqlService,
+                new DatabaseServiceFactory(sqlService),
                 commandLineArguments,
-                args
+                new XmlProducerUsingDOM(),
+                new LayoutFolder(SchemaAnalyzer.class.getClassLoader()),
+                new ScExceptionChecked(
+                    urlBuilder,
+                    new ScNullChecked(
+                        urlBuilder,
+                        new ScSimple(
+                            connectionConfig,
+                            urlBuilder,
+                            new DriverFromConfig(connectionConfig)
+                        )
+                    )
+                )
+            ),
+            commandLineArguments,
+            args
         );
     }
 
