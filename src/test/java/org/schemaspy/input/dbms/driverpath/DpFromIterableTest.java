12b113b77c2fbf14430b03dbf00a226b76d9281e
Nils Petzall <nils.petzall@gmail.com>
2024-01-16 00:03:32 +0100

    chore(dp): add subpaths to driverpath

--- src/test/java/org/schemaspy/input/dbms/driverpath/DpFromIterableTest.java
+++ src/test/java/org/schemaspy/input/dbms/driverpath/DpFromIterableTest.java
@@ -1,34 +1,50 @@
 package org.schemaspy.input.dbms.driverpath;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DpFromIterableTest {
 
+  private static final Path basePath = Paths.get("src", "test", "resources", "subPaths");
+  @BeforeAll
+  static void createEmptyDir() throws IOException {
+    Path emptyDir = basePath.resolve("emptyDir");
+    if (!emptyDir.toFile().exists()) {
+      Files.createDirectories(emptyDir);
+    }
+  }
+
   @Test
   void somePathsAndAMissing() {
     assertThat(
       new DpFromIterable(
         List.of(
-          Paths.get("src", "main"),
-          Paths.get("src", "test"),
+          basePath,
           Paths.get("src", "nobueno")
         )
       ).value()
     ).isEqualTo(
-      "src"
-      + File.separator
-      + "main"
-      + File.pathSeparator
-      + "src"
-      + File.separator
-      + "test"
+      List.of(
+        basePath,
+        basePath.resolve("emptyDir"),
+        basePath.resolve("other"),
+        basePath.resolve("other").resolve("otherSub"),
+        basePath.resolve("other").resolve("otherSub").resolve("otherSub.properties"),
+        basePath.resolve("some.properties")
+      ).stream()
+        .map(Path::toString)
+        .collect(Collectors.joining(File.pathSeparator))
     );
   }
 
