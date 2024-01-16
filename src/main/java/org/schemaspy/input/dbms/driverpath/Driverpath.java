dd96bbd643948cfcd9b99349e1a3bed02925a5e8
Nils Petzall <nils.petzall@gmail.com>
2024-01-16 19:30:26 +0100

    chore(driverpath): remove default method value()

--- src/main/java/org/schemaspy/input/dbms/driverpath/Driverpath.java
+++ src/main/java/org/schemaspy/input/dbms/driverpath/Driverpath.java
@@ -1,26 +1,10 @@
 package org.schemaspy.input.dbms.driverpath;
 
-import java.io.File;
 import java.nio.file.Path;
-
-import org.schemaspy.util.Mapped;
 
 /**
  * Abstracts a location to a JDBC driver.
  */
 public interface Driverpath extends Iterable<Path> {
 
-    /**
-     * Asks the driver path to represent itself in text.
-     * @return A textual representation of the JDBC driver location.
-     */
-    default String value() {
-        return String.join(
-          File.pathSeparator,
-          new Mapped<>(
-            this,
-            Path::toString
-          )
-        );
-    }
 }
