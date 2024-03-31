f3aa85768378dde0a851223ab2c3f321671ec73f
Jesper Olsson <software@jesperolsson.se>
2024-03-31 21:20:08 +0200

    refactor(name): consider null

--- src/main/java/org/schemaspy/util/naming/NameFromString.java
+++ src/main/java/org/schemaspy/util/naming/NameFromString.java
@@ -1,15 +1,19 @@
 package org.schemaspy.util.naming;
 
-public class NameFromString implements Name {
+import java.util.Objects;
+
+public final class NameFromString implements Name {
 
     private final String value;
 
-    public NameFromString(String value) {
+    public NameFromString(final String value) {
         this.value = value;
     }
 
     @Override
     public String value() {
-        return value;
+        return !Objects.isNull(this.value)
+            ? this.value
+            : new EmptyName().value();
     }
 }
