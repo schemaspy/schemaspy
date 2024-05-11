e12cc5fc1dcc9cfebdc8feac652225cb00773724
Jesper Olsson <software@jesperolsson.se>
2024-05-11 08:17:59 +0200

    refactor(xml): extract class

--- src/main/java/org/schemaspy/input/dbms/xml/TableMeta.java
+++ src/main/java/org/schemaspy/input/dbms/xml/TableMeta.java
@@ -55,22 +55,9 @@ public class TableMeta {
         NamedNodeMap attribs = tableNode.getAttributes();
 
         name = attribs.getNamedItem("name").getNodeValue();
+        comments = new CmFacade(tableNode).value();
 
-        Node node = attribs.getNamedItem("comments");
-        if (node == null) {
-            node = attribs.getNamedItem("remarks");
-            if (Objects.nonNull(node)) {
-                LOGGER.warn("<remarks> has been deprecated");
-            }
-        }
-        if (node != null) {
-            String tmp = node.getNodeValue().trim();
-            comments = tmp.length() == 0 ? null : tmp;
-        } else {
-            comments = null;
-        }
-
-        node = attribs.getNamedItem("remoteSchema");
+        Node node = attribs.getNamedItem("remoteSchema");
         remoteSchema = node == null ? null : node.getNodeValue().trim();
 
         node = attribs.getNamedItem("remoteCatalog");
